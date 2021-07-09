4f0151beb10948bfc509e65ab972a0dc04a96141
Sebastian <tassone_af@yahoo.com>
2021-07-09 00:28:20 -0300

    refactor styles

--- src/app/services/heros.service.ts
+++ src/app/services/heros.service.ts
@@ -1,23 +1,21 @@
 import { Injectable } from '@angular/core';
 
-export interface Hero{
-  id:number;
-  nombre: string,
-  bio: string,
-  img: string,
-  aparicion: string,
-  casa: string,
-  
+export interface Hero {
+  id: number;
+  nombre: string;
+  bio: string;
+  img: string;
+  aparicion: string;
+  casa: string;
 }
 
 @Injectable()
 export class HerosService {
-
   private heros: Hero[] = [
     {
       id: 0,
       nombre: 'Aquaman',
-      bio: 'El poder más reconocido de Aquaman es la capacidad telepática para comunicarse con la vida marina, la cual puede convocar a grandes distancias.',
+      bio: 'Aquaman is most recognized power is the telepathic ability to communicate with marine life, which it can summon over great distances.',
       img: 'assets/resources/aquaman.png',
       aparicion: '1941-11-01',
       casa: 'DC',
@@ -25,7 +23,7 @@ export class HerosService {
     {
       id: 1,
       nombre: 'Batman',
-      bio: 'Los rasgos principales de Batman se resumen en «destreza física, habilidades deductivas y obsesión». La mayor parte de las características básicas de los cómics han variado por las diferentes interpretaciones que le han dado al personaje.',
+      bio: 'The main features of Batman are summarized in "physical prowess, deductive abilities and obsession." Most of the basic characteristics of the comics have varied by the different interpretations that have been given to the character.',
       img: 'assets/resources/batman.png',
       aparicion: '1939-05-01',
       casa: 'DC',
@@ -33,7 +31,7 @@ export class HerosService {
     {
       id: 2,
       nombre: 'Daredevil',
-      bio: 'Al haber perdido la vista, los cuatro sentidos restantes de Daredevil fueron aumentados por la radiación a niveles superhumanos, en el accidente que tuvo cuando era niño. A pesar de su ceguera, puede "ver" a través de un "sexto sentido" que le sirve como un radar similar al de los murciélagos.',
+      bio: "Having lost his sight, Daredevil's remaining four senses were heightened by radiation to superhuman levels, in the accident he had as a child. Despite his blindness, he can 'see' through a 'sixth sense' that serves as a bat-like radar.",
       img: 'assets/resources/daredevil.png',
       aparicion: '1964-01-01',
       casa: 'Marvel',
@@ -41,7 +39,7 @@ export class HerosService {
     {
       id: 3,
       nombre: 'Hulk',
-      bio: 'Su principal poder es su capacidad de aumentar su fuerza hasta niveles prácticamente ilimitados a la vez que aumenta su furia. Dependiendo de qué personalidad de Hulk esté al mando en ese momento (el Hulk Banner es el más débil, pero lo compensa con su inteligencia).',
+      bio: 'His main power is his ability to increase his strength to virtually unlimited levels while increasing his fury. Depending on which Hulk personality is in command at the time (the Hulk Banner is the weakest, but he makes up for it with his intelligence).',
       img: 'assets/resources/hulk.png',
       aparicion: '1962-05-01',
       casa: 'Marvel',
@@ -49,7 +47,7 @@ export class HerosService {
     {
       id: 4,
       nombre: 'Linterna Verde',
-      bio: 'Poseedor del anillo de poder que posee la capacidad de crear manifestaciones de luz sólida mediante la utilización del pensamiento. Es alimentado por la Llama Verde (revisada por escritores posteriores como un poder místico llamado Starheart), una llama mágica contenida en dentro de un orbe (el orbe era en realidad un meteorito verde de metal que cayó a la Tierra, el cual encontró un fabricante de lámparas llamado Chang)',
+      bio: 'Possessor of the ring of power that possesses the ability to create manifestations of solid light through the use of thought. It is powered by the Green Flame (revised by later writers as a mystical power called Starheart), a magical flame contained within an orb (the orb was actually a green metal meteorite that fell to Earth, which a manufacturer found of lamps named Chang)',
       img: 'assets/resources/linterna-verde.png',
       aparicion: '1940-06-01',
       casa: 'DC',
@@ -57,7 +55,7 @@ export class HerosService {
     {
       id: 5,
       nombre: 'Spider-Man',
-      bio: 'Tras ser mordido por una araña radiactiva, obtuvo los siguientes poderes sobrehumanos, una gran fuerza, agilidad, poder trepar por paredes. La fuerza de Spider-Man le permite levantar 10 toneladas o más. Gracias a esta gran fuerza Spider-Man puede realizar saltos íncreibles. Un "sentido arácnido", que le permite saber si un peligro se cierne sobre él, antes de que suceda. En ocasiones este puede llevar a Spider-Man al origen del peligro.',
+      bio: 'After being bitten by a radioactive spider, he obtained the following superhuman powers, great strength, agility, being able to climb walls. Spider-Mans strength allows him to lift 10 tons or more. Thanks to this great strength, Spider-Man can perform incredible jumps. A "spider sense" that lets him know if a danger is hanging over him, before it happens Sometimes this can lead Spider-Man to the source of danger.',
       img: 'assets/resources/spiderman.png',
       aparicion: '1962-08-01',
       casa: 'Marvel',
@@ -65,7 +63,7 @@ export class HerosService {
     {
       id: 6,
       nombre: 'Wolverine',
-      bio: 'En el universo ficticio de Marvel, Wolverine posee poderes regenerativos que pueden curar cualquier herida, por mortal que ésta sea, además ese mismo poder hace que sea inmune a cualquier enfermedad existente en la Tierra y algunas extraterrestres . Posee también una fuerza sobrehumana, que si bien no se compara con la de otros superhéroes como Hulk, sí sobrepasa la de cualquier humano.',
+      bio: 'In the fictional universe of Marvel, Wolverine has regenerative powers that can heal any wound, no matter how deadly it may be, and that same power makes him immune to any disease existing on Earth and some aliens. He also possesses superhuman strength, which although it does not compare with that of other superheroes like the Hulk, it does surpass that of any human.',
       img: 'assets/resources/wolverine.png',
       aparicion: '1974-11-01',
       casa: 'Marvel',
@@ -74,15 +72,15 @@ export class HerosService {
 
   constructor() {}
 
-  getHeros():Hero[]{
-    return this.heros
+  getHeros(): Hero[] {
+    return this.heros;
   }
-  getHeroById(i:number):Hero | undefined{
-    return this.heros.find(hero => hero.id == i)
+  getHeroById(i: number): Hero | undefined {
+    return this.heros.find((hero) => hero.id == i);
   }
 
-  searchHeros(input:string):Hero[] {
+  searchHeros(input: string): Hero[] {
     input = input.toLowerCase();
-    return this.heros.filter(e => e.nombre.toLowerCase().includes(input))
+    return this.heros.filter((e) => e.nombre.toLowerCase().includes(input));
   }
 }
